f23247ddd332ef0005739d9649f814fad9acebef
Wilson Wu <wilsoncwu@gmail.com>
2020-03-27 00:59:04 +0000

    init view: :

--- battlegame/urls.py
+++ battlegame/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from playerdata import views
 urlpatterns = [
+    path('', views.test_view),
     path('admin/', admin.site.urls),
 ]
