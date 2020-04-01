36399c41f79a8cc3a062c7e9e96b30e921e81975
Wilson Wu <wilsoncwu@gmail.com>
2020-04-01 16:22:38 +0000

    base characters

--- playerdata/models.py
+++ playerdata/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BaseCharacter(models.Model):
+    char_id = models.AutoField(primary_key=True)
+    health = models.IntegerField()
+    mana = models.IntegerField()
+    speed = models.IntegerField()
+    attack = models.IntegerField()
+    ar = models.IntegerField()
+    mr = models.IntegerField()
+    attack_range = models.IntegerField()
+    rarity = models.IntegerField()
+    crit_chance = models.IntegerField()
