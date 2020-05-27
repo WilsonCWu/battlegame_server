eafa92bd18328d045b3f7135393a2a2caf578157
Wilson Wu <wilsoncwu@gmail.com>
2020-05-27 00:27:02 +0000

    video party"

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,9 @@
 # chat/routing.py
 from django.urls import re_path
 from . import consumers
+from . import videoparty
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/videoparty/(?P<room_name>\w+)/$', videoparty.PartyConsumer),
 ]
