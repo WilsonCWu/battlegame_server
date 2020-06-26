c91e09e84bd4e8e77c11f40646062c3aed1199b1
Daniel Wang <danielw68@gmail.com>
2020-06-26 14:34:04 -0400

    Add documentation

--- battlegame/cron.py
+++ battlegame/cron.py
@@ -5,12 +5,27 @@ from playerdata.models import ActiveWeeklyQuest
 from playerdata.models import PlayerQuestDaily
 from playerdata.models import PlayerQuestWeekly
 
+"""
+For reference:
+Add/update cron job: `python manage.py crontab add`
+Remove all jobs:" `python manage.py crontab remove`
+
+To check what jobs are actually scheduled on crontab: `crontab -l`
+Double check that crontab on the server is running on UTC timezone
+"""
+
+
+# https://stackoverflow.com/questions/57167237/how-to-delete-first-n-items-from-queryset-in-django
+def _delete_first_n_rows(quest_class, n):
+    quest_class.objects.filter(
+        id__in=list(quest_class.objects.values_list('pk', flat=True)[:n])).delete()
+
 
 def daily_quests_cron():
     # remove top 3 from daily
     print("running daily quest reset cronjob")
-    ActiveDailyQuest.objects.filter(id__in=list(ActiveDailyQuest.objects.values_list('pk', flat=True)[:constants.NUM_DAILY_QUESTS])).delete()
-    # remove all playerquests that are from there
+
+    _delete_first_n_rows(ActiveDailyQuest, constants.NUM_DAILY_QUESTS)
     PlayerQuestDaily.objects.all().delete()
 
     # pull new ones and make them for every user
@@ -30,8 +45,8 @@ def daily_quests_cron():
 def weekly_quests_cron():
     # remove top 5 from weekly
     print("running weekly quest reset cronjob")
-    ActiveWeeklyQuest.objects.filter(id__in=list(ActiveWeeklyQuest.objects.values_list('pk', flat=True)[:constants.NUM_WEEKLY_QUESTS])).delete()
-    # remove all playerquests that are from there
+
+    _delete_first_n_rows(ActiveWeeklyQuest, constants.NUM_WEEKLY_QUESTS)
     PlayerQuestWeekly.objects.all().delete()
 
     # pull new ones and make them for every user
