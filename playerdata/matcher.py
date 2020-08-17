0ac833d0fff390655cd172569903d1f3801ad2e7
Daniel Wang <danielw68@gmail.com>
2020-08-17 10:11:25 -0700

    Add tourney elo

--- playerdata/matcher.py
+++ playerdata/matcher.py
@@ -49,6 +49,7 @@ class TeamSchema(Schema):
 class UserInfoSchema(Schema):
     user_id = fields.Int(attribute='user_id')
     elo = fields.Int()
+    tourney_elo = fields.Int()
     name = fields.Str()
     profile_picture = fields.Int()
     num_wins = fields.Int(attribute='user.userstats.num_wins')
