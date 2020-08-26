4310e417d7aaa16976c3fd064c30939ad79b7e51
Yan Ke <yansongke1@gmail.com>
2020-08-26 19:29:09 -0400

    Fix readme + test

--- playerdata/tests/README.md
+++ playerdata/tests/README.md
@@ -1,3 +1,8 @@
 # Tests
 
-`ALTER USER u_battlegame CREATEDB;`
+To run tests, you can use `./manage.py test`. There is a fixture file generated
+from a JSON dump in `playerdata/tests/fixtures.json` that has basic items
+and characters.
+
+Postgres may require the `u_battlegame` user to have createdb permissions. This
+can be granted through `ALTER USER u_battlegame CREATEDB;`.
--- playerdata/tests/test_inventory.py
+++ playerdata/tests/test_inventory.py
@@ -1,27 +1,35 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from playerdata.models import User, Character
+from playerdata.models import User, BaseCharacter, Character, BaseItem, Item
 
 class EquipItemAPITestCase(APITestCase):
     fixtures = ['playerdata/tests/fixtures.json']
 
     def setUp(self):
-        u = User.objects.get(username='battlegame')
-        self.client.force_authenticate(user=u)
+        self.u = User.objects.get(username='battlegame')
+        self.client.force_authenticate(user=self.u)
 
     def test_equipping_item(self):
-        # TODO: let's make this test proper (not just using seemingly magical
-        # char and item IDs) once we have a change to make proper test
-        # fixtures. Right now we're just using a JSON dump for the
-        # current database, but we probably just want something with the base
-        # character and items.
+        base_bow = BaseItem.objects.get(name="Bow")
+        base_archer = BaseCharacter.objects.get(name="Archer")
+
+        owned_bow = Item.objects.create(
+            user = self.u,
+            item_type = base_bow,
+            exp = 0,
+        )
+        owned_archer = Character.objects.create(
+            user = self.u,
+            char_type = base_archer,
+        )
+
         response = self.client.post('/equipitem/', {
             'target_slot': 'W',
-            'target_char_id': 11,
-            'target_item_id': 11,
+            'target_char_id': owned_archer.char_id,
+            'target_item_id': owned_bow.item_id,
         })
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        equipped_char = Character.objects.get(char_id=11)
-        self.assertEqual(equipped_char.weapon_id, 11)
+        owned_archer.refresh_from_db()
+        self.assertEqual(owned_archer.weapon, owned_bow)
