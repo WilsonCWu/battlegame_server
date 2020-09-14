d971c1e0c7ea3ce191ae880f07fe0ec7c62386ad
Daniel Wang <danielw68@gmail.com>
2020-09-14 10:34:45 -0400

    make a dot file

--- battlegame/settings.py
+++ battlegame/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SERVICE_ACCOUNT_FILE = '/home/battlegame/battlegame/google-service-account.json'
+SERVICE_ACCOUNT_FILE = '/home/battlegame/battlegame/.google-service-account.json'
 
 DEVELOPMENT = config('DEVELOPMENT', False)
 
