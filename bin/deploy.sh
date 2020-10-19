a3e7949a8d9992d1687a2cbe7af8684e5df0065d
Yan Ke <yansongke1@gmail.com>
2020-10-18 21:28:31 -0400

    add scripts, update CI (#89)

---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+set -eo pipefail
+
+cd /home/battlegame/battlegame
+source /home/battlegame/venv/bin/activate
+
+git checkout master && git pull -f
+pip install -r requirements.txt
+python /home/battlegame/battlegame/manage.py migrate
+deactivate
+
+supervisorctl restart asgi:asgi0 asgi:asgi1 asgi:asgi2 asgi:asgi3 battlegame
