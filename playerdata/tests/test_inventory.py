d89a511b0cadaa0649f181c3ad07e15f6bc27f2e
Daniel Wang <danielw68@gmail.com>
2020-12-14 21:09:45 -0500

    Fix test lmao

--- playerdata/tests/test_inventory.py
+++ playerdata/tests/test_inventory.py
@@ -196,4 +196,4 @@ class LevelUpAPITestCase(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertFalse(response.data['status'])
-        self.assertIn('already hit level 170', response.data['reason'])
+        self.assertIn('already hit max level 170', response.data['reason'])
