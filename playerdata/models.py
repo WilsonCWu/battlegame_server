ccd668afdf6222bdf6798f2b24404aa648528420
Daniel Wang <danielw68@gmail.com>
2021-01-20 18:30:49 -0500

    patch notes (#209)

--- playerdata/models.py
+++ playerdata/models.py
@@ -39,6 +39,7 @@ class ServerStatus(models.Model):
     # Fields for Maintenance events.
     maintenance_start = models.DateTimeField(blank=True, null=True)
     expected_end = models.DateTimeField(blank=True, null=True)
+    patch_notes = models.TextField(default="", blank=True)
 
     class Meta:
         indexes = [
