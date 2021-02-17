513be383fcc6ad32397bc554034150ede804bfad
Yan Ke <yansongke1@gmail.com>
2021-02-17 18:38:23 -0500

    Fix fixtures

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 cd /home/battlegame/battlegame
 source /home/battlegame/venv/bin/activate
 
-python /home/battlegame/battlegame/manage.py dumpdata --exclude auth.permission --exclude contenttypes > /home/circleci/db.json
+python /home/battlegame/battlegame/manage.py dumpdata --exclude auth.permission --exclude contenttypes --exclude admin.logentry > /home/circleci/db.json
 
 deactivate
