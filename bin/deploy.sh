c7b17a31b3fac4618b2362674522e4da00b604c0
Yan Ke <yansongke1@gmail.com>
2021-03-11 17:43:27 -0500

    Update deploy.sh (#284)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,6 @@ source /home/battlegame/venv/bin/activate
 git checkout master && git pull -f
 pip install -r requirements.txt
 python /home/battlegame/battlegame/manage.py migrate
-python /home/battlegame/battlegame/manage.py crontab add
 deactivate
 
 supervisorctl restart asgi:asgi0 asgi:asgi1 asgi:asgi2 asgi:asgi3 battlegame
