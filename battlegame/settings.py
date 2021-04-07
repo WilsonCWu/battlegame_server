f583bc6b2a7fcefafa7b9a287fe19257679c7cd6
Yan Ke <yansongke1@gmail.com>
2021-04-07 00:59:13 -0400

    Update settings.py (#321)

--- battlegame/settings.py
+++ battlegame/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# TODO: consolidate this with DEVELOPMENT flag.
+DEBUG = config('DEVELOPMENT', False)
 
 SERVICE_ACCOUNT_FILE = '/home/battlegame/battlegame/.google-service-account.json'
 
