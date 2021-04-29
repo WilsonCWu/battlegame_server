5488fca3cd572bf1817d82a46f8cec3a79604bc3
Daniel Wang <danielw68@gmail.com>
2021-04-29 18:09:43 -0400

    all out baby

--- playerdata/tier_system.py
+++ playerdata/tier_system.py
@@ -1,26 +1,11 @@
+import math
+
 from playerdata import constants
 
 
 def elo_to_tier(elo: int):
-    if elo < 250:
-        return constants.Tiers.BRONZE_TWO
-    elif elo < 500:
-        return constants.Tiers.BRONZE_ONE
-    elif elo < 750:
-        return constants.Tiers.SILVER_TWO
-    elif elo < 1000:
-        return constants.Tiers.SILVER_ONE
-    elif elo < 1300:
-        return constants.Tiers.GOLD_TWO
-    elif elo < 1600:
-        return constants.Tiers.GOLD_ONE
-    elif elo < 2000:
-        return constants.Tiers.PLAT_TWO
-    elif elo < 2300:
-        return constants.Tiers.PLAT_ONE
-    elif elo < 2600:
-        return constants.Tiers.DIAMOND_TWO
-    elif elo < 3000:
-        return constants.Tiers.DIAMOND_ONE
+    division_increment = 100
+    if elo < constants.Tiers.MASTER.value * 100:
+        return constants.Tiers(math.floor(elo / division_increment) + 1)
     else:
         return constants.Tiers.MASTER
