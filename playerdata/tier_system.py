3d73028baddd6662effdeaa16ae67a79876d5173
Daniel Wang <danielw68@gmail.com>
2021-04-30 11:35:45 -0400

    int type instead of tier for elo pegging

--- playerdata/tier_system.py
+++ playerdata/tier_system.py
@@ -12,5 +12,5 @@ def elo_to_tier(elo: int):
 
 #  we peg the rewards to the middle elo of each tier
 #  Ex: If you're at Bronze 4 (100-199 elo), the rewards are pegged at 150 elo
-def get_tier_reward_elo(tier: constants.Tiers):
-    return (tier.value * constants.TIER_ELO_INCREMENT) - constants.TIER_ELO_INCREMENT / 2
+def get_tier_reward_elo(tier: int):
+    return (tier * constants.TIER_ELO_INCREMENT) - constants.TIER_ELO_INCREMENT / 2
