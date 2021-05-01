5a596ed47472fcf66c263f51590ae5b7e9d66ea2
Daniel Wang <danielw68@gmail.com>
2021-05-01 18:54:21 -0400

    referrer 500 (#358)
    * referral 500
    
    * rename
    
    * referer substract
    
    * website

--- playerdata/referral.py
+++ playerdata/referral.py
@@ -15,8 +15,8 @@ class ReferralSchema(Schema):
     referral_code = fields.Str()
 
 
-def award_referral(user):
-    user.inventory.gems += constants.REFFERAL_GEMS_REWARD
+def award_referral(user, amount):
+    user.inventory.gems += amount
     user.inventory.save()
 
 
@@ -61,13 +61,13 @@ class ReferralView(APIView):
                 repeat_referral.user.save()
 
             # subtract earned gems from the repeat accounts on the user_ref side
-            user_ref.user.inventory.gems -= num_converted * constants.REFFERAL_GEMS_REWARD
+            user_ref.user.inventory.gems -= num_converted * constants.REFERER_GEMS_REWARD
             user_ref.user.inventory.save()
 
             return Response({'status': False, 'reason': 'Your account has been banned due to suspicion of fraud. If you believe this is a mistake, please contact our customer support.'})
 
         # Award requesting user
-        award_referral(request.user)
+        award_referral(request.user, constants.REFEREE_GEMS_REWARD)
         ReferralTracker.objects.create(user=request.user, referral=user_ref, device_id=device_id)
 
         return Response({'status': True})
