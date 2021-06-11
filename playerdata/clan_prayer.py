6eabe3688a0052974d293591a481421005dfd112
Yan Ke <yansongke1@gmail.com>
2021-06-11 17:32:22 -0400

    remove unused var

--- playerdata/clan_prayer.py
+++ playerdata/clan_prayer.py
@@ -7,6 +7,3 @@ https://docs.google.com/document/d/1Dxsy1OfBRzbHIEXHfrZZf45SjSatXJ8RTqZ81W5NKdw/
 # TODO:
 # /status endpoint for a player
 # /pray endpoint to submit a prayer
-
-def empty_prayers():
-    return [{}, {}, {}, {}, {}, {}, {}]
