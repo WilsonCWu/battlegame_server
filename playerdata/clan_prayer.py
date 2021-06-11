211a4c69df6b129781a9b4f0e9d41430e268ad12
Yan Ke <yansongke1@gmail.com>
2021-06-11 17:31:34 -0400

    Prayer model

---
+++ playerdata/clan_prayer.py
@@ -0,0 +1,12 @@
+"""Clan Prayers
+
+Design doc:
+https://docs.google.com/document/d/1Dxsy1OfBRzbHIEXHfrZZf45SjSatXJ8RTqZ81W5NKdw/edit
+"""
+
+# TODO:
+# /status endpoint for a player
+# /pray endpoint to submit a prayer
+
+def empty_prayers():
+    return [{}, {}, {}, {}, {}, {}, {}]
