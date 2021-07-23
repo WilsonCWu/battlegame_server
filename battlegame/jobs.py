d1fb8ddf2858563314ec15c2159321cecced2c41
Daniel Wang <danielw68@gmail.com>
2021-07-23 11:05:37 -0400

    add backfill job

--- battlegame/jobs.py
+++ battlegame/jobs.py
@@ -2,6 +2,7 @@
 
 from django.db import transaction
 
+from playerdata import formulas
 from playerdata.models import *
 
 
@@ -147,3 +148,42 @@ def backfill_highest_seasonelo():
 def backfill_storymode():
     for user in User.objects.all():
         _, _ = StoryMode.objects.get_or_create(user=user)
+
+
+@transaction.atomic
+def backfill_vip_levels():
+    userinfos = UserInfo.objects.all().select_related('user__inventory')
+    for userinfo in userinfos:
+        player_level = formulas.exp_to_level(userinfo.player_exp)
+
+        if player_level < 30:
+            exp = 0
+        elif player_level < 40:
+            exp = 100
+        elif player_level < 50:
+            exp = 300
+        elif player_level < 60:
+            exp = 1000
+        elif player_level < 70:
+            exp = 2000
+        elif player_level < 80:
+            exp = 4000
+        elif player_level < 90:
+            exp = 7000
+        elif player_level < 100:
+            exp = 10000
+        elif player_level < 110:
+            exp = 14000
+        elif player_level < 120:
+            exp = 20000
+        else:
+            exp = 30000
+
+        userinfo.vip_exp = exp
+
+        # backfill the purchased gems if the person bought something more than the free deal
+        pt = PurchasedTracker.objects.filter(user=userinfo.user)
+        if pt.count() > 1:
+            userinfo.vip_exp += userinfo.user.inventory.gems_bought
+
+    UserInfo.objects.bulk_update(userinfos, ['vip_exp'])
