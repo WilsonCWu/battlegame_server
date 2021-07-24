bf7758f8afcf9e33ef0628d08532bf01bd96496d
Daniel Wang <danielw68@gmail.com>
2021-07-24 18:20:52 -0400

    afk multiplier

--- playerdata/afk_rewards.py
+++ playerdata/afk_rewards.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from playerdata.formulas import afk_coins_per_min, afk_exp_per_min, afk_dust_per_min
+from playerdata.formulas import afk_coins_per_min, afk_exp_per_min, afk_dust_per_min, vip_exp_to_level
 from playerdata.models import DungeonProgress
 
 
@@ -29,8 +29,9 @@ class GetAFKRewardView(APIView):
         dust_per_min = afk_dust_per_min(dungeon_progress.campaign_stage)
         # exp_per_min = afk_exp_per_min(dungeon_progress.stage_id)
 
-        coins = math.floor(time * coins_per_min)
-        dust = math.floor(time * dust_per_min)
+        vip_level = vip_exp_to_level(request.user.userinfo.vip_exp)
+        coins = math.floor(time * coins_per_min * afk_rewards_multiplier_vip(vip_level))
+        dust = math.floor(time * dust_per_min * afk_rewards_multiplier_vip(vip_level))
         # exp = time * exp_per_min
 
         return Response({'coins_per_min': coins_per_min,
@@ -52,8 +53,9 @@ class CollectAFKRewardView(APIView):
         dungeon_progress = DungeonProgress.objects.get(user=request.user)
 
         time = mins_since_last_collection(last_collected_time)
-        coins = math.floor(time * afk_coins_per_min(dungeon_progress.campaign_stage))
-        dust = math.floor(time * afk_dust_per_min(dungeon_progress.campaign_stage))
+        vip_level = vip_exp_to_level(request.user.userinfo.vip_exp)
+        coins = math.floor(time * afk_coins_per_min(dungeon_progress.campaign_stage) * afk_rewards_multiplier_vip(vip_level))
+        dust = math.floor(time * afk_dust_per_min(dungeon_progress.campaign_stage) * afk_rewards_multiplier_vip(vip_level))
         # exp = time * afk_exp_per_min(dungeon_progress.stage_id)
 
         inventory.last_collected_rewards = datetime.now(timezone.utc)
@@ -65,3 +67,44 @@ class CollectAFKRewardView(APIView):
         # request.user.userinfo.save()
 
         return Response({'status': True, 'coins': coins, 'dust': dust})
+
+
+def afk_rewards_multiplier_vip(level: int):
+    if level == 1:
+        return 1.05
+    elif level == 2:
+        return 1.1
+    elif level == 2:
+        return 1.15
+    elif level == 3:
+        return 1.2
+    elif level == 4:
+        return 1.25
+    elif level == 5:
+        return 1.3
+    elif level == 6:
+        return 1.5
+    elif level == 7:
+        return 1.55
+    elif level == 8:
+        return 1.6
+    elif level == 9:
+        return 1.9
+    elif level == 10:
+        return 2
+    elif level == 11:
+        return 2.1
+    elif level == 12:
+        return 2.5
+    elif level == 13:
+        return 2.6
+    elif level == 14:
+        return 3
+    elif level == 15:
+        return 3.3
+    elif level == 16:
+        return 3.6
+    elif level >= 17:
+        return 4
+    else:
+        return 1
