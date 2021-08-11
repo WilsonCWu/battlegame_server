1da852a5a090e8156c439594e3ed58c1ffdf1e35
Daniel Wang <danielw68@gmail.com>
2021-08-10 22:22:21 -0400

    password settings

--- battlegame/settings.py
+++ battlegame/settings.py
@@ -118,6 +118,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6380/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": config('REDIS_PVP_PASSWORD')
+        }
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
