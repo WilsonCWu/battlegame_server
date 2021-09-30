a2df7160d7a95ea9414852a6530a16dec0577d04
Daniel Wang <danielw68@gmail.com>
2021-09-30 11:30:43 -0400

    matchmaking player pool (#533)
    * open player pool
    
    * cap 1000 and only bots for beginners

--- playerdata/pvp_queue.py
+++ playerdata/pvp_queue.py
@@ -75,9 +75,14 @@ def _get_opponents_within_elo(cur_elo, search_count, exclude_list):
     min_elo = cur_elo - bound
     max_elo = cur_elo + bound
 
-    return UserInfo.objects.filter(elo__gte=min_elo, elo__lte=max_elo, user__dungeonprogress__campaign_stage__gt=5) \
-                .exclude(user_id__in=exclude_list) \
-                .values_list('user_id', flat=True)[:50]
+    if max_elo <= 150:
+        return UserInfo.objects.filter(elo__gte=min_elo, elo__lte=max_elo, is_bot=True) \
+            .exclude(user_id__in=exclude_list) \
+            .values_list('user_id', flat=True)[:1000]
+    else:
+        return UserInfo.objects.filter(elo__gte=min_elo, elo__lte=max_elo,) \
+            .exclude(user_id__in=exclude_list) \
+            .values_list('user_id', flat=True)[:1000]
 
 
 def get_opponents_list(user, exclude_list):
