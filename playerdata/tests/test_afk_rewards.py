3793cbc75805a048d6cc58b422925b3cdd7a465d
Daniel Wang <danielw68@gmail.com>
2021-10-03 16:09:07 -0400

    collect afk

--- playerdata/tests/test_afk_rewards.py
+++ playerdata/tests/test_afk_rewards.py
@@ -33,3 +33,27 @@ class AFKRewardsAPITestCase(APITestCase):
         self.u.afkreward.refresh_from_db()
         self.assertEqual(self.u.afkreward.runes_to_be_converted, 0)
         self.assertEqual(self.u.afkreward.runes_left, 0)
+
+    def test_collect_afk(self):
+        d1 = datetime.today().replace(tzinfo=timezone.utc) - timedelta(days=1)
+
+        self.u.afkreward.runes_left = 3000
+        self.u.afkreward.last_eval_time = d1
+        self.u.afkreward.save()
+
+        orginal_coins = self.u.inventory.coins
+
+        response = self.client.get('/afkrewards/get')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['status'])
+
+        response = self.client.post('/afkrewards/collect')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['status'])
+
+        self.u.afkreward.refresh_from_db()
+        self.u.inventory.refresh_from_db()
+        self.assertEqual(self.u.afkreward.unclaimed_gold, 0)
+        self.assertEqual(self.u.afkreward.unclaimed_converted_runes, 0)
+        self.assertTrue(self.u.inventory.coins > orginal_coins)
