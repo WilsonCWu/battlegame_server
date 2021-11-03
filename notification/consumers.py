011aed0620d8a230859723dd6d2a6f5801887e57
Daniel Wang <danielw68@gmail.com>
2021-11-03 14:33:13 -0400

    comment out logs

--- notification/consumers.py
+++ notification/consumers.py
@@ -46,14 +46,12 @@ def notif_channel_group_name(user_id):
 
 # Channel group is the user_id
 # Only 1 channel per group, but this is still the convention since group names
-# are user defined, channel id's are not
+# are user defined, channel id's are auto generated and un-gettable
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope["user"]
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = notif_channel_group_name(self.room_name)
-
-        print(f'Group name {self.room_group_name}')
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -63,12 +61,13 @@ class NotificationConsumer(WebsocketConsumer):
 
         self.accept()
 
-        # TODO: we can move all our notif badge counts to redis, except qureying db for the init
         # Send current notification badge counts
-        self.send(text_data=json.dumps({
-            'notif_type': 12,
-            'amount': 32
-        }))
+        
+        # Test send
+        # self.send(text_data=json.dumps({
+        #     'notif_type': 12,
+        #     'amount': 32
+        # }))
 
     def disconnect(self, close_code):
         # Leave room group
@@ -79,11 +78,9 @@ class NotificationConsumer(WebsocketConsumer):
 
     # Don't receive anything from other side only use this websocket to push new notification badge numbers
     def receive(self, text_data):
-        print(text_data)
         pass
 
     # sends notification amounts to the client socket
     def push_notif(self, event):
-        print(f"push {event['data']}")
-
+        # print(f"push {event['data']}")
         self.send(text_data=json.dumps(event['data']))
--- notification/tests.py
+++ notification/tests.py
@@ -1,17 +1,3 @@
-from channels.testing import WebsocketCommunicator
 from django.test import TestCase
 
-from notification.consumers import NotificationConsumer
-
-
-class MyTests(TestCase):
-    async def test_my_consumer(self):
-        communicator = WebsocketCommunicator(NotificationConsumer, "/ws/notif/12/")
-        connected, subprotocol = await communicator.connect()
-        assert connected
-        # Test sending text
-        await communicator.send_to(text_data="hello")
-        response = await communicator.receive_from()
-        assert response == "hello"
-        # Close
-        await communicator.disconnect()
+# Create your tests here.
