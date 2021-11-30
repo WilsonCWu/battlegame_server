07fe1fee32d89b3de898a5b1891bd7877a22b93b
shaunlazaro <shaunlazaro@gmail.com>
2021-11-30 16:43:11 -0500

    graph formatting, graph functions return context, add sums

--- battlegame/figures.py
+++ battlegame/figures.py
@@ -1,6 +1,5 @@
-import pandas
+import pandas as pd
 from plotly import graph_objects
-from pandas import DataFrame
 from dataclasses import dataclass
 
 from playerdata import constants
@@ -12,21 +11,34 @@ def get_sample_graph():
         [graph_objects.Bar(y=[1, 2, 3])],
         layout_title_text="Sample Graph using plotly"
     )
-    return data.to_html()
+    context = {'graph_contents': data.to_html()}
+    return context
+
+
+def get_sample_graphs():
+    context_list = [get_sample_graph(), get_sample_graph(), get_sample_graph()]
+    graph_contents = [context['graph_contents'] for context in context_list]
+    return {'graph_contents': graph_contents}
 
 
 def get_usage_stats_graph():
     df = get_base_character_usage_dataframe(BaseCharacterUsage.objects.all())
+    pd.options.plotting.backend = "plotly"
+
+    context = {'other_data': []}  # What we'll be returning
     names = df['name']
     df = df.loc[:, df.columns.str.contains('bucket')]  # We only need bucket data
-    pandas.options.plotting.backend = "plotly"
 
     # Make another column for each bucket.
     bucket_count = constants.NUMBER_OF_USAGE_BUCKETS
     for index in range(bucket_count):
+        games_bucket_sum = df[f'games bucket {index}'].sum()
+        def_games_bucket_sum = df[f'defense games bucket {index}'].sum()
         # Divide total games by 5 (even though not all teams have 5 heroes, almost all of them should)
-        df[f'off usage bucket {index}'] = 100 * df[f'games bucket {index}'] / (df[f'games bucket {index}'].sum()/5)
-        df[f'def usage bucket {index}'] = 100 * df[f'defense games bucket {index}'] / (df[f'defense games bucket {index}'].sum()/5)
+        df[f'off usage bucket {index}'] = 100 * df[f'games bucket {index}'] / (games_bucket_sum/5)
+        df[f'def usage bucket {index}'] = 100 * df[f'defense games bucket {index}'] / (def_games_bucket_sum/5)
+        if games_bucket_sum + def_games_bucket_sum != 0:
+            context['other_data'].append(f'Bucket {index} games: {games_bucket_sum}, defense games: {games_bucket_sum}')
 
     # Remove old columns
     df.drop(df.columns[df.columns.str.contains('games bucket')], axis=1, inplace=True)
@@ -36,47 +48,50 @@ def get_usage_stats_graph():
 
     graphs = []  # This needs to be a list of functions
 
+    # remove 'off/def bucket ' by removing first 17 characters, then take that as int and multiply by bucket size to get elo
+    # format as 'elo - (next elo - 1)'
+    def get_graph_column_name(s):  # eg. s = 'off bucket 17' -> '8500 - 8999'
+        bucket_num = int(s[17:])
+        return f'{bucket_num*constants.USAGE_BUCKET_ELO_SIZE} - {(bucket_num+1)*constants.USAGE_BUCKET_ELO_SIZE-1}'
+
     # Split dataframe into defense and offense
     # We want to get 2 lists of the buckets with win rates (1 for def, 1 for off)
     df_offense = df.loc[:, df.columns.str.contains('off')]
-    df_offense = df_offense.rename(columns=lambda s: s[10:])  # Remove "off usage " so it groups with the defense data in the graph
+    df_offense = df_offense.rename(columns=get_graph_column_name)  # Remove "off usage " so it groups with the defense data in the graph
     df_defense = df.loc[:, df.columns.str.contains('def')]
-    df_defense = df_defense.rename(columns=lambda s: s[10:])  # Remove "def usage " so it groups with the offense data in the graph
-
-    highest_wr = df.max(numeric_only=True).max()  # Set the y axis max to the highest winrate
+    df_defense = df_defense.rename(columns=get_graph_column_name)  # Remove "def usage " so it groups with the offense data in the graph
 
     # Take each row of the dataframe and plot it (facet_row = "variable" makes it 2 bar graphs together)
     for index in range(len(names)):
         offense_data = df_offense.loc[index, :]
         defense_data = df_defense.loc[index, :]
-        graph_df = DataFrame(dict(offense=offense_data, defense=defense_data))
+        graph_df = pd.DataFrame(dict(offense=offense_data, defense=defense_data))
         fig = graph_df.plot.bar(facet_row="variable", title=names[index], width=800,
+                                color_discrete_sequence=['red', 'blue'],
                                 labels=dict(index="bucket", value="usage rate", variable="game type"))
-        fig.update_yaxes(range=[0, highest_wr])
+        fig.update_yaxes(range=[0, 100])
         graphs.append(fig.to_html)
-    return graphs
+    context['graph_title'] = 'Usage Rate Graphs'
+    context['graph_contents'] = graphs
+    return context
 
 
 # Each entry should be a list of functions
-# Each function should return an html string or a list of html strings
-GRAPHS = {'sample': [get_sample_graph],  # Single graph example
-          'samples': [get_sample_graph, get_sample_graph, get_sample_graph],  # Multiple graphs example
-          'usage': [get_usage_stats_graph]}
+# Each function should return a dictionary with graph_contents being the graph or a list of graphs.
+# dictionary can also have an 'other_data' field.
+GRAPHS = {'sample': get_sample_graph,  # Single graph example
+          'samples': get_sample_graphs,  # Multiple graphs example
+          'usage': get_usage_stats_graph}
 
 
 # Gives context for rendering a graph or list of graphs with the graphs.html template
 def get_graph_context(name):
-    graph_list = []
+    context = {}
     if name in GRAPHS:
-        for graph_function in GRAPHS[name]:
-            func_contents = graph_function()
-            if isinstance(func_contents, list):  # Function returns list of graphs
-                graph_list.extend(func_contents)
-            else:  # Function returns one graph
-                graph_list.append(func_contents)
-    context = {'graph_title': 'Graph',
-               'graph_contents': graph_list,
-               'other_graph_links': GRAPHS.keys()}
+        context = GRAPHS[name]()
+    if 'graph_title' not in context:
+        context['graph_title'] = 'Graph'
+    context['other_graph_links'] = GRAPHS.keys()
     return context
 
 
@@ -119,5 +134,5 @@ def get_base_character_usage_dataframe(queryset):
                          wins_buckets=base_char_usage.num_wins_buckets,
                          defense_games_buckets=base_char_usage.num_defense_games_buckets,
                          defense_wins_buckets=base_char_usage.num_defense_wins_buckets))
-    df = DataFrame([character.to_dict() for character in char_data])
+    df = pd.DataFrame([character.to_dict() for character in char_data])
     return df
