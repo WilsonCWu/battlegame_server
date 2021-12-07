cfe3071a8e7f4e56e1de99ae9daa9a60f8539a14
shaunlazaro <shaunlazaro@gmail.com>
2021-12-07 14:27:34 -0500

    reformat query

--- playerdata/matcher.py
+++ playerdata/matcher.py
@@ -325,11 +325,9 @@ class GetReplayMatchView(APIView):
     def get(self, request):
         serializer = IntSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        match_q = Match.objects.filter(id=serializer.validated_data['value'])
-        if not match_q:
+        match = Match.objects.filter(id=serializer.validated_data['value']).first()
+        if match is None:
             return Response({'status': False, 'reason': 'match expired or does not exist'})
-
-        match = match_q[0]
         if ServerStatus.latest_version() != match.version:
             return Response({'status': False, 'reason': 'replay version out of date'})
 
