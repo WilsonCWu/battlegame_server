255b9274affea188c7dc39960a774ac3974c085c
Daniel Wang <danielw68@gmail.com>
2022-02-12 14:30:18 -0500

    support multiple world packs

--- playerdata/world_pack.py
+++ playerdata/world_pack.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from playerdata.models import WorldPack
+from rest_marshmallow import Schema, fields
 
 
 def get_world_expiration():
@@ -13,30 +14,90 @@ def get_world_expiration():
     return curr_time + timedelta(days=3)
 
 
+class Pack:
+    def __init__(self, purchase_id, rewards, bonus_value):
+        self.purchase_id = purchase_id
+        self.bonus_value = bonus_value
+        self.rewards = rewards
+
+
+class PackSchema(Schema):
+    purchase_id = fields.Str()
+    bonus_value = fields.Int()
+    rewards = fields.Nested(chests.ChestRewardSchema, many=True)
+
+
+# TODO: Tune
 # these rewards are "wrapped", i.e. the rarity of the chest instead of the contents of the chest
-def get_world_pack_rewards(user):
+def active_world_packs(user):
     rewards = []
     world = user.worldpack.world
-    if world < 10:
-        gems = (world * 300) + 1500
-        coins = (world * 30000) + 25000
 
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value))
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.GEMS.value, value=gems))
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=coins))
+    if world <= 1:
+        pack_1 = Pack(constants.WORLD_PACK_0,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=6),
+                       chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=4),
+                       chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=25000)],
+                      510)
+        pack_2 = Pack(constants.WORLD_PACK_1,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=6),
+                       chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=4),
+                       chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=25000)],
+                      510)
+        pack_3 = Pack(constants.WORLD_PACK_2,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=6),
+                       chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=4),
+                       chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=25000)],
+                      510)
+        rewards.extend([pack_1, pack_2, pack_3])
+
+    elif world < 10:
+        gems = (world * 300) + 1500
+        coin_hours = 42
+        dust_hours = 42
+
+        pack_1 = Pack(constants.WORLD_PACK_1,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value),
+                       chests.ChestReward(reward_type=constants.RewardType.GEMS.value, value=gems),
+                       chests.ChestReward(reward_type=constants.RewardType.COINS_FAST_REWARDS.value, value=coin_hours)],
+                      510)
+        pack_2 = Pack(constants.WORLD_PACK_2,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value),
+                       chests.ChestReward(reward_type=constants.RewardType.GEMS.value, value=gems),
+                       chests.ChestReward(reward_type=constants.RewardType.DUST_FAST_REWARDS.value, value=dust_hours)],
+                      510)
+        # pack_3 = Pack(constants.WORLD_PACK_3,
+        #               [chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=6),
+        #                chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=4),
+        #                chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=25000)])
+
+        rewards.extend([pack_1, pack_2])
+
     else:
         dust_hours = 12 + (world // 2) * 2  # starts at 22, +2 every two worlds
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.LEGENDARY.value))
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value))
-        rewards.append(chests.ChestReward(reward_type=constants.RewardType.DUST_FAST_REWARDS.value, value=dust_hours))
+
+        # pack_1 = Pack(constants.WORLD_PACK_1,
+        #               [chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=6),
+        #                chests.ChestReward(reward_type=constants.RewardType.CHAR_ID.value, value=4),
+        #                chests.ChestReward(reward_type=constants.RewardType.COINS.value, value=25000)])
+        # pack_2 = Pack(constants.WORLD_PACK_2,
+        #               [chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.LEGENDARY.value),
+        #                chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value),
+        #                chests.ChestReward(reward_type=constants.RewardType.DUST_FAST_REWARDS.value, value=dust_hours)],
+        #               510)
+        pack_3 = Pack(constants.WORLD_PACK_3,
+                      [chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.LEGENDARY.value),
+                       chests.ChestReward(reward_type=constants.RewardType.CHEST.value, value=constants.ChestType.MYTHICAL.value),
+                       chests.ChestReward(reward_type=constants.RewardType.DUST_FAST_REWARDS.value, value=dust_hours)],
+                      470)
+        rewards.extend([pack_3])
 
     return rewards
 
 
-def active_new_pack(user, world: int):
+def activate_new_pack(user, world: int):
     user.worldpack.world = world
     user.worldpack.expiration_date = get_world_expiration()
-    user.worldpack.is_claimed = False
     user.worldpack.save()
 
 
@@ -58,10 +119,9 @@ class GetWorldPack(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        active_pack = get_world_pack_rewards(request.user)
+        active_packs = active_world_packs(request.user)
 
         return Response({'status': True,
-                         'world_pack': chests.ChestRewardSchema(active_pack, many=True).data,
+                         'world_packs': PackSchema(active_packs, many=True).data,
                          'expiration_time': request.user.worldpack.expiration_date,
-                         'purchase_id': get_purchase_id(request.user.worldpack)
                          })
