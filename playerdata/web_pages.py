2036fac036dcd733cf7bb55fbeeb07e0dd585ff5
Daniel Wang <danielw68@gmail.com>
2022-03-23 19:41:26 -0400

    redeem form

--- playerdata/web_pages.py
+++ playerdata/web_pages.py
@@ -1,28 +1,82 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from playerdata.models import User, ClaimedCode, BaseCode, Mail
 
 from playerdata import constants
+from playerdata.forms import RedeemInboxForm
+from playerdata.login import UserRecoveryTokenGenerator
 from playerdata.models import BaseCharacter
 
 
 def privacy(request):
     return render(request, 'privacy.html', {})
 
+
 def terms(request):
     return render(request, 'terms.html', {})
+
 
 def install(request):
     return render(request, 'install.html', {})
 
+
 def beta(request):
     return redirect("https://forms.gle/Xv7Bfx1a7fPjSU1E8")
+
 
 def about(request):
     return render(request, 'about.html', {})
 
+
+def redeem(request):
+    if request.method == 'POST':
+        form = RedeemInboxForm(request.POST)
+        if form.is_valid():
+            otp = form.cleaned_data['otp']
+            code = form.cleaned_data['code']
+
+            user_id_index = otp.find("-")
+            if user_id_index == -1:
+                return render(request, 'redeem.html', {'error': 'Invalid token, try copying it again from Settings > Transfer'})
+
+            user_id = otp[:user_id_index]  # Grab the userid from the otp token
+            token = otp[user_id_index + 1:]  # Grab the rest from the otp token
+
+            try:
+                user = User.objects.get(id=user_id)
+            except:
+                return render(request, 'redeem.html', {'error': 'Invalid token, try copying it again from Settings > Transfer'})
+
+            generator = UserRecoveryTokenGenerator()
+            if not generator.check_token(user, token):
+                return render(request, 'redeem.html', {'error': 'Invalid token, try copying it again from Settings > Transfer'})
+
+            basecode = BaseCode.objects.filter(code=code).first()
+            if basecode is None:
+                return render(request, 'redeem.html', {'error': 'Invalid code'})
+
+            # if not already claimed token
+            if ClaimedCode.objects.filter(user=user, code=basecode).exists():
+                return render(request, 'redeem.html', {'error': 'Invalid code, already claimed'})
+
+            # send inbox
+            sender_id = 1
+            sender = User.objects.get(id=sender_id)
+            pfp_id = sender.userinfo.profile_picture
+
+            Mail.objects.create(title="Redeem Your Code", message="Claim your rewards!\n\nBattle on adventurer!",
+                                sender_id=sender_id, receiver_id=user_id,
+                                code=basecode, sender_profile_picture_id=pfp_id,
+                                has_unclaimed_reward=True)
+            return render(request, 'redeem.html', {'success': 'Check your in-game Inbox to claim your rewards!'})
+
+    return render(request, 'redeem.html', {})
+
+
 def chest_droprate(chest_type, rarity, num_rarity):
     rarity_odds = constants.REGULAR_CHAR_ODDS_PER_CHEST[chest_type - 1][rarity - 1] / 1000
     return round(rarity_odds / num_rarity, 3)
+
 
 def info(request):
     mythical_droprates = []
@@ -39,7 +93,6 @@ def info(request):
             "rarity": char.to_rarity_name,
             "rate": chest_droprate(constants.ChestType.MYTHICAL.value, char.rarity, num_rarity[char.rarity - 2])
         })
-
 
     return render(request, 'info.html', {
         'mythical_droprates': mythical_droprates,
