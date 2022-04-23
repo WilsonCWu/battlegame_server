9aa85390bcc0cf6462a4a01215a2436e42ec4041
Daniel Wang <danielw68@gmail.com>
2022-04-23 00:33:29 -0400

    add masilda

--- playerdata/story_mode.py
+++ playerdata/story_mode.py
@@ -8,7 +8,7 @@ from playerdata import chests, constants, base
 from playerdata.models import StoryQuest
 from playerdata.serializers import IntSerializer, CharStateResultSerializer
 
-CHARACTER_POOLS = [[1, 4]]  # TODO: more on the way as etilon works on dialogue
+CHARACTER_POOLS = [[1, 4, 12]]  # TODO: more on the way as etilon works on dialogue
 CHAR_POOL_CAMPAIGN_STAGE_UNLOCK = [24]  # stage that unlocks the respective character pool tiers
 MAX_NUM_QUESTS = 5
 
